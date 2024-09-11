96576548398b838275fffe04b0bba939ee07d399
chai <chaithawat.contact@gmail.com>
2024-09-11 10:48:09 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chaithawat Pinsuwan on 11/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
